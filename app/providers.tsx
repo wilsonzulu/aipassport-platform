0be0a564a5dfe296cbc19f1921331c44f7b1d5bb
Wilson Zulu <wilsonzulu1984@gmail.com>
2026-03-06 14:14:54 +0200

    improve wallet connection config

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,13 +1,13 @@
 "use client";
 
 import * as React from "react";
-import { WagmiProvider, createConfig, http } from "wagmi";
+import { WagmiProvider, http } from "wagmi";
 import { polygonAmoy } from "wagmi/chains";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import {
   RainbowKitProvider,
-  getDefaultWallets,
   darkTheme,
+  getDefaultConfig,
 } from "@rainbow-me/rainbowkit";
 
 import "@rainbow-me/rainbowkit/styles.css";
@@ -15,13 +15,9 @@ import "@rainbow-me/rainbowkit/styles.css";
 const projectId =
   process.env.NEXT_PUBLIC_WC_PROJECT_ID || "fccd3aa241cc4f67f943df31e454a170";
 
-const { connectors } = getDefaultWallets({
+const config = getDefaultConfig({
   appName: "AI Meta Passport",
   projectId,
-});
-
-const config = createConfig({
-  connectors,
   chains: [polygonAmoy],
   transports: {
     [polygonAmoy.id]: http(),
@@ -33,7 +29,7 @@ const queryClient = new QueryClient();
 
 export default function Providers({ children }: { children: React.ReactNode }) {
   return (
-    <WagmiProvider config={config}>
+    <WagmiProvider config={config} reconnectOnMount>
       <QueryClientProvider client={queryClient}>
         <RainbowKitProvider theme={darkTheme()} modalSize="compact">
           {children}
